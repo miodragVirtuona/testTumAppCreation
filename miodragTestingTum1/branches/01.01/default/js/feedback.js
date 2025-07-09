5d821169bad2de7bb3710f4a25f8d1f9db36defc
LOCAL SERVICE <LOCAL SERVICE@DESKTOP-SO3TUGA>
2025-07-09 11:38:40 +0200

    desc

---
+++ miodragTestingTum1/branches/01.01/default/js/feedback.js
@@ -0,0 +1,13 @@
+if(APP.APP_NAMES) {
+	APP.APP_NAMES.FEEDBACK = {
+		"uri": "http://dev.virtuona.rs/virtdev_scas/resources/TasorUnitManagerAppKB/RIDd395ab84-c753-4f8f-9f14-de106b06301c",
+		"name": "https://www.planmeout.com/virtuona/TWCFeedbackdefault"
+	}
+} else {
+	APP.APP_NAMES = {
+		"FEEDBACK": {
+			"uri": "http://dev.virtuona.rs/virtdev_scas/resources/TasorUnitManagerAppKB/RIDd395ab84-c753-4f8f-9f14-de106b06301c",
+			"name": "https://www.planmeout.com/virtuona/TWCFeedbackdefault"
+		}
+	}
+}
