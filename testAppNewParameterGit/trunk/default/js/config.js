9ff1db5f6b5dec8105cf5e4a607d6d7356d124d4
LOCAL SERVICE <LOCAL SERVICE@DESKTOP-SO3TUGA>
2025-09-29 13:21:33 +0200

    desc

---
+++ testAppNewParameterGit/trunk/default/js/config.js
@@ -0,0 +1,14 @@
+var APP = {
+	FU_URI: "",
+	EXISTING_LANGUAGES : ["en"],
+	LANGUAGE_RESOURCES: {
+		"en": "http://localhost/testing_scas/resources/testAppNewParameterGitAppType/en.json"
+	},
+	DEFAULT_LANGUAGE : "en",
+	APP_TYPE_PROJ : "http://localhost/testing_scas/resources/TasorProjects/testAppNewParameterGitAppType",
+	APP_KB_PROJ : "http://localhost/testing_scas/resources/TasorProjects/testAppNewParameterGitAppKB",
+	APP_ONTOS_PROJ : "http://localhost/testing_scas/resources/TasorProjects/testAppNewParameterGitAppOntos",
+	APP_KB_WDS: "http://localhost/testing_scas/resources/TasorProjects/testAppNewParameterGitAppKB_WDS",
+    APP_KB_DELETE_DS: "http://localhost/testing_scas/resources/TasorProjects/testAppNewParameterGitAppKB_WDS_deletedResources",
+	EXT_LINKS : {},
+};
